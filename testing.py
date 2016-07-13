028720caa2d79dd6736a21bda02873f1a2b9dd83
ClaudiaWinklmayr <claudia.winklmayr@gmx.net>
2016-07-13 18:05:52 +0200

    added tests for sonar()

--- testing.py
+++ testing.py
@@ -1,19 +1,21 @@
 import moving_bat as mb
 import numpy as np
 
-mb.plot_trajectory()
+#mb.plot_trajectory()
 
-sen1=mb.generate_sensors()
+#sen1=mb.generate_sensors()
 sen2=mb.generate_sensors(2,'orthogonal')
 #sen3=mb.generate_sensors(3,'orthogonal')
-sen4=mb.generate_sensors(5)
-sen5=mb.generate_sensors(3,'test')
+#sen4=mb.generate_sensors(5)
+#sen5=mb.generate_sensors(3,'test')
 
-print('arg=None', sen1)
-print('arg=2, orthogonal', sen2)
-print(np.linalg.norm(sen4[:,1]))
+#print('arg=None', sen1)
+#print('arg=2, orthogonal', sen2)
+#print(np.linalg.norm(sen4[:,1]))
 #print('arg=3, orthogonal', sen3)
-print('arg=5', sen4)
-print('arg=3, test', sen5)
+#print('arg=5', sen4)
+#print('arg=3, test', sen5)
 
+
+print(mb.sonar((1,1), sen2))
 
