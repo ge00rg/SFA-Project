287d28b24296720e43b1e8927b48723787432b6a
Georg Chechelnizki <georg.chechelnizki@rub.de>
2016-08-02 14:58:36 +0200

    Fixed outpu dim bug

--- sfa_tasks.py
+++ sfa_tasks.py
@@ -14,7 +14,7 @@ def train_sfa(series, poly_exp=1, out_dim=1, whiten=False, svd=False):
         extracted from series using the SFA algorithm
     '''
     flow = (mdp.nodes.PolynomialExpansionNode(poly_exp) +
-            mdp.nodes.SFANode(output_dim=out_dim))
+            mdp.nodes.SFANode())
 
     if whiten:
         flow.insert(0,mdp.nodes.WhiteningNode(svd=svd, reduce=True))
@@ -40,18 +40,20 @@ def mesh(sen, data, flow, spacing=0.1, draw=True):
 
     retuns: the whole room treated with the trained sfa, ndarray.
     '''
+    od = flow[-1].get_output_dim()
+    print(od)
     x, y, grid = mb.generate_grid_data(sen, spacing=spacing)
 
     dim_temp = len(x)*len(y)
-    grid_temp = np.reshape(grid, (dim_temp, sen.shape[1]))
+    grid_temp = np.reshape(grid, (dim_temp, od))
 
     slow = execute_sfa(flow, grid_temp)
 
-    slow_reshape = np.reshape(slow, (len(x), len(y), sen.shape[1]))
+    slow_reshape = np.reshape(slow, (len(x), len(y), od))
 
     if draw:
         rng = np.max([mb.ROOMLENGTH, mb.ROOMWIDTH])
-        for i in range(sen.shape[1]):
+        for i in range(od):
             plt.imshow(slow_reshape[:,:,i], vmin=-rng, vmax=rng, cmap='plasma')
             plt.colorbar()
             plt.show()
